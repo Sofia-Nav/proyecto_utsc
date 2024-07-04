48085acb053985a2675acc862aea9d2754dba803
Sofia-Nav <157772830+Sofia-Nav@users.noreply.github.com>
2024-07-04 03:54:32 +0000

    se agregaron los archivos

--- model/db_con.php
+++ model/db_con.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("America/Mexico_City");
 class conexion{
     public function get_connection(){
-        $connection = mysqli_connect("0.tcp.ngrok.io","root","","mlogic","17831");
+        $connection = mysqli_connect("db","mariadb","","mariadb","mariadb");
         $connection -> set_charset("utf8");
         return $connection;
     }
