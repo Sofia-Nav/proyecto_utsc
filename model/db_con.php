8a5f70f2a1a08be499e0df36df9e7fc011fddb33
Sofia-Nav <157772830+Sofia-Nav@users.noreply.github.com>
2024-07-06 02:08:56 +0000

    se agregaron los nuevos cambios

--- model/db_con.php
+++ model/db_con.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("America/Mexico_City");
 class conexion{
     public function get_connection(){
-        $connection = mysqli_connect("db","mariadb","","mariadb","mariadb");
+        $connection = mysqli_connect("db","mariadb","mariadb","mariadb");
         $connection -> set_charset("utf8");
         return $connection;
     }
